556e5790bb1bdea29583b8d85f1407f3a02bc321
Arthur <arthur.pulfermuller@gmail.com>
2024-03-19 16:46:36 +0100

    Ajout fonction like d'un post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,6 +2,7 @@ const postModel = require("../models/post.model");
 const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId; // Pour manipuler les ID de MongoDB
+
 //
 //
 // Fonction de création d'un post______________________________________________
@@ -86,4 +87,51 @@ module.exports.deletePost = async (req, res) => {
     // Si erreur, log de l'erreur
     console.log("Failed to delete :" + err);
   }
+};
+
+//
+//
+// Fonction pour liker un post_________________________________________________
+module.exports.likePost = async (req, res) => {
+  // Vérification de la validité de l'id passé en paramètre de la requête
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("Unknown ID : " + req.params.id);
+
+  try {
+    // Tentative de mise à jour du post dans la base de données
+    const post = await PostModel.findByIdAndUpdate(
+      req.params.id,
+      { $addToSet: { likers: req.body.id } },
+      { new: true }
+    );
+
+    // Si une erreur se produit lors de la mise à jour du post, une réponse avec un statut 400 est renvoyée, contenant l'erreur
+    if (!post) return res.status(400).send("Post not found");
+
+    // Tentative de mise à jour de l'utilisateur dans la base de données
+    const user = await UserModel.findByIdAndUpdate(
+      req.body.id,
+      { $addToSet: { likes: req.params.id } },
+      { new: true }
+    );
+
+    // Si une erreur se produit lors de la mise à jour de l'utilisateur, une réponse avec un statut 400 est renvoyée, contenant l'erreur
+    if (!user) return res.status(400).send("User not found");
+
+    // Si tout se passe bien, une réponse avec un statut 200 est renvoyée, contenant le post et l'utilisateur mis à jour
+    return res.status(200).json({ post, user });
+  } catch (err) {
+    // Si une erreur se produit lors de l'exécution du code, une réponse avec un statut 400 est renvoyée, contenant l'erreur
+    return res.status(400).send(err);
+  }
+};
+
+
+//
+//
+// Fonction pour unliker un post_________________________________________________
+module.exports.unlikePost = async (req, res) => {
+  // Vérification de la validité de l'id passé en paramètre de la requête
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("Unknown ID : " + req.params.id);
 };
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -6,5 +6,8 @@ router.get("/", postController.readPost);
 router.post("/", postController.createPost);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
+// Gestion des likes/unlikes
+router.patch('/like-post/:id', postController.likePost)
+router.patch('/unlike-post/:id', postController.unlikePost)
 
 module.exports = router;
