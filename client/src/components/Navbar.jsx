d59d0a5a651f560e0956d66ae1cf0717f9cea93a
Arthur <arthur.pulfermuller@gmail.com>
2024-04-04 16:19:23 +0200

    intégration de redux en cours

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { UidContext } from "./AppContext";
+import Logout from "./Logout";
 
 const Navbar = () => {
-    return (
-        <div>
-            TEST
+  const uid = useContext(UidContext);
+
+  
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo">
+          <NavLink to="/">
+            <div className="logo">
+              <img src="./img/icon.png" alt="logo du site" />
+              <h3>Raccoont</h3>
+            </div>
+          </NavLink>
         </div>
-    );
+        {uid ? (
+          <ul>
+            <li></li>
+            <li className="welcome">
+              <NavLink to="/profil">
+                <h5>Bienvenue</h5>
+              </NavLink>
+            </li>
+            <Logout />
+          </ul>
+        ) : (
+          <ul>
+            <li></li>
+            <li>
+              <NavLink to="/profil">
+                <img src="./img/icons/login.svg" alt="icon connexion" />
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "./styles/index.scss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { thunk } from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// DEV TOOLS
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { logger } from "redux-logger";
+
+// Ancienne méthode => corriger par plus récente !!!
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
+  <Provider store={store}>
     <App />
-
+  </Provider>
 );
